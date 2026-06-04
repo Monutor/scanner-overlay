724a17d6dbf98fed7dffdaa3b5b5da2218531dd9
Monutor <sporte2222@yandex.ru>
2026-06-04 18:20:34 +0300

    v1.9.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.scanner.overlay"
         minSdk = 26
         targetSdk = 36
-        versionCode = 17
-        versionName = "1.8.0"
+        versionCode = 18
+        versionName = "1.9.0"
     }
 
     signingConfigs {
