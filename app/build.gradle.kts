dae91d9bc504fdb179c12dd0304ed4de800a906d
Monutor <sporte2222@yandex.ru>
2026-07-28 08:26:09 +0300

    v1.17.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.scanner.overlay"
         minSdk = 26
         targetSdk = 36
-        versionCode = 26
-        versionName = "1.16.0"
+        versionCode = 27
+        versionName = "1.17.0"
         var token = ""
         val propsFile = rootProject.file("local.properties")
         if (propsFile.exists()) {
